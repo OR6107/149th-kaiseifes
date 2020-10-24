1cb6b0037f61813c3ab5e6e43955b9643c6421b1
Mencotton <mencotton0410@gmail.com>
2020-10-24 19:26:42 +0900

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'eslint:recommended',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
