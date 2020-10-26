08211e500af4732f446d09315a5be61ada2f6849
Mencotton <mencotton0410@gmail.com>
2020-10-26 18:06:52 +0900

    Change the favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,9 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.JPG' }, // ここを変更する
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
