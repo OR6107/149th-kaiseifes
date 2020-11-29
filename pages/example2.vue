b2a9c11c4e19394513fadf07a97e66033c24f01e
OR6107 <landriver07@gmail.com>
2020-11-29 22:21:54 +0900

    Created Coming Soon Page

--- pages/example2.vue
+++ pages/example2.vue
@@ -1,62 +1,99 @@
 <template>
-  <div>
-    <h1>Hello! {{ name }}!!!</h1>
-    上が赤くなってるのはassetsのmain.scssが効いているから〜
-    <div class="container-didot">
-      <div class="title">Didot</div>
-      <div class="subtitle">blue text</div>
+  <div class="container">
+    <div class="title">
+      <p class="sorit">創立148周年記念</p>
+      <p class="kaiseisai">開成祭</p>
     </div>
-    <div class="container-helvetica">
-      <div class="title">Helvetica</div>
+    <div class="whendes">
+      <p class="when">開催予定</p>
+      <p class="date">
+        2019.09.21(土) 9:00-17:00 <br />2019.09.22(日) 9:00-17:00
+      </p>
     </div>
-    <div class="container-din">
-      <div class="title">DIN</div>
+    <div class="plzwait">
+      148th開成祭公式サイトは８月下旬の公開に向け鋭意準備中です。<br />
+      公開までの間は開成祭公式ツイッターをご覧ください。<br />
+      <a href="https://twitter.com/kaiseisai"
+        ><div class="buttons"><p>Twitterをみる</p></div></a
+      >
     </div>
-    <div class="container-udnewgothic">
-      <div class="title">UD新ゴ</div>
-    </div>
+    <!---
+    <article>
+      <h2>“Pick Up 文化祭” とは？</h2>
+      <div>・文化祭を統括する文化祭準備委員会(文準)や、中１や高１がクラスで行う団体(HR参団)などの文化祭準備の様子を不定期に紹介
+        <br>・文化祭に参加する団体(参団)が書いた、その団体のテーマに関連した記事がいっぱい
+      </div>
+      <p>とりあえず見ましょう.</p>
+      <div class="buttons">Pick Up! 文化祭をみる！→</div>
+    </article>
+    --->
   </div>
 </template>
-
 <script>
+import Logo from '~/components/Logo.vue'
 export default {
-  data() {
-    return {
-      name: 'World',
-    }
+  components: {
+    Logo,
   },
 }
 </script>
-
-<style lang="scss" scoped>
-$title-size: 100px;
-
-div[class^='container'] {
-  font-style: normal;
-  font-weight: normal;
+<style>
+/*a{*/
+/*  color: #ccbd14*/
+/*}*/
+.container {
+  padding: 0px 15px 30px;
+  background-color: #411445;
+  color: white;
+}
+.sorit {
+  font-size: 24px;
+  font-weight: bold;
+}
+.kaiseisai {
+  font-size: 48px;
+  font-weight: bold;
+}
+.title {
+  padding: 80px 0px;
+  margin: 0px 40px;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   text-align: center;
-  .title {
-    font-size: $title-size;
-  }
 }
-
-.container-didot {
-  font-family: linotype-didot, serif;
-  .subtitle {
-    color: blue;
-    font-size: 20px;
-  }
+.sorit {
 }
-
-.container-helvetica {
-  font-family: neue-haas-grotesk-display, sans-serif;
+.kaiseisai {
 }
-
-.container-din {
-  font-family: din-2014, sans-serif;
+.whendes {
+  height: 18vh;
 }
-
-.container-udnewgothic {
-  font-family: a-otf-ud-shin-go-pr6n, sans-serif;
+.when {
+  font-size: 20px;
+  text-align: center;
+}
+.date {
+  font-size: 22px;
+  font-weight: bold;
+  text-align: center;
+}
+.plzwait {
+  text-align: center;
+}
+.buttons {
+  margin: 20px auto;
+  height: 40px;
+  width: 80%;
+  border-radius: 5px;
+  background-color: #912a55;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,165 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">149th-kaiseifes</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="menu">
+      <div class="Rectangle17"></div>
     </div>
+    <div class="logo"></div>
+    <div class="twilight">Twilight</div>
+    <div class="Ellipse3"></div>
+    <div class="Ellipse4"></div>
+    <div class="when">
+      開催日時
+      <br />
+      2021.01.09(土)09:00〜17:00
+      <br />
+      2021.01.10(日)09:00〜16:30
+    </div>
+    <div class="coming-soon">Coming Soon...</div>
   </div>
 </template>
-
 <script>
-export default {}
+import Logo from '~/components/Logo.vue'
+export default {
+  components: {
+    Logo,
+  },
+}
 </script>
-
 <style>
+/*フォントはUD新ゴ*/
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
+  position: relative;
+  width: 1440px;
+  height: 1024px;
+
+  /* White */
+  background: #ffffff;
+}
+
+.logo {
+  position: absolute;
+  width: 825px;
+  height: 808px;
+  left: -25px;
+  top: 75px;
+
+  background: url(logo-red.svg);
+  background-blend-mode: darken;
+}
+
+.when {
+  position: absolute;
+  width: 675px;
+  height: 214px;
+  left: 765px;
+  top: 387px;
+
+  font-family: a-otf-ud-shin-go-pr6n, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 42px; /*48pxだったが途中で改行されてしまうため変更*/
+  line-height: 71px;
+  text-align: center;
+
+  /* Black */
+  color: #082449;
+}
+.coming-soon {
+  position: absolute;
+  width: 603px;
+  height: 109px;
+  left: 786px;
+  top: 735px;
+
+  font-family: a-otf-ud-shin-go-pr6n, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 72px;
+  line-height: 106px;
+  text-align: center;
+
+  /* Black */
+  color: #082449;
+}
+
+.twilight {
+  position: absolute;
+  width: 380px;
+  height: 142px;
+  left: 191px;
+  top: 790px;
+
+  font-family: a-otf-ud-shin-go-pr6n, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 96px;
+  line-height: 142px;
+  /* identical to box height */
+
+  /* Black */
+  color: #082449;
+}
+
+.Ellipse4 {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  left: 750px;
+  top: 555px;
+
+  /* Yellow */
+  background: #fccf5a;
+}
+
+.Ellipse3 {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  left: 750px;
+  top: 487px;
+
+  /* Yellow */
+  background: #fccf5a;
+}
+
+.menu {
+  position: absolute;
+  width: 1440px;
+  height: 60px;
+  left: 0px;
+  top: 0px;
+}
+
+.Rectangle17 {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  /* Purple */
+  background: #611fb6;
+}
+
+. menulist {
+  /*menulistは非表示*/
+  position: absolute;
+  visibility: hidden;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: a-otf-ud-shin-go-pr6n, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 53px;
   display: flex;
-  justify-content: center;
   align-items: center;
   text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  /* White */
+  color: #ffffff;
 }
 </style>
