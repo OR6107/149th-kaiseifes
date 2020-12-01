c8a71cede82abfba80272994770a1a77c35913f9
Mencotton <mencotton0410@gmail.com>
2020-12-01 16:29:39 +0900

    Add Header

---
+++ components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="header">
+      <Hamburger class="hamburger" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Hamburger from '@/components/hamburger'
+
+export default {
+  name: 'Header',
+  components: { Hamburger },
+}
+</script>
+
+<style lang="scss" scoped>
+$black: #082449;
+$dark: #38444d;
+$gray: #888888;
+$light: #e5e5e5;
+$white: #ffffff;
+$purple: #611fb6;
+$orange: #ff9a70;
+$yellow: #fccf5a;
+
+.header {
+  background-color: $purple;
+  color: $white;
+  height: 60px;
+  padding: 5px;
+  display: flex;
+}
+
+.hamburger {
+  margin: auto 10px auto auto;
+}
+</style>
