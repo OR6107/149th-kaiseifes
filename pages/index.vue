37d1ce7662a0b0f7b071947f08b8ca0aaf63479b
Mencotton <mencotton0410@gmail.com>
2020-12-01 23:21:39 +0900

    Update CSS
    闇

--- pages/index.vue
+++ pages/index.vue
@@ -68,7 +68,7 @@ $yellow: #fccf5a;
   @include center();
   @include Didot-font();
   width: calc(min(1vw, 2vh) * 45);
-  margin-top: calc((100vh - calc(min(1vw, 2vh) * (42.5 - 4.5 + 5 + 5 + 10))) / 2);
+  margin-top: calc((100vh - calc(min(1vw, 2vh) * 55)) / 2);
   margin-left: 5vw;
   img {
     height: calc(min(1vw, 2vh) * 42.5);
@@ -87,22 +87,23 @@ $yellow: #fccf5a;
   @include center();
   @include UD-sin-go-font();
   width: calc(min(1vw, 2vh) * 40);
-  margin-top: calc((100vh - calc(min(1vw, 2vh) * (3.5 + 5 + 2.5 + 4.2 + 4 + 5.3))) / 2);
+  margin-top: calc((100vh - calc(min(1vw, 2vh) * 35)) / 2);
   margin-left: auto;
   margin-right: 5vw;
+  color: $black;
   .when-title {
     margin-right: calc(min(1vw, 2vh) * 2.5);
     font-size: calc(min(1vw, 2vh) * 3.5);
     line-height: calc(min(1vw, 2vh) * 5);
-    color: $black;
   }
   .date {
     text-align: center;
+    width: calc(min(1vw, 2vh) * 40);
     font-size: calc(min(1vw, 2vh) * 2.5);
     line-height: calc(min(1vw, 2vh) * 4.2);
     color: $yellow;
     .date-text {
-      margin-left: calc(min(1vw, 2vh) * -5);
+      margin-left: auto;
       color: $black;
     }
   }
@@ -125,12 +126,11 @@ $yellow: #fccf5a;
   }
   .coming-soon {
     @include Didot-font();
-    margin-top: calc(min(1vw, 2vh) * 8);
+    margin-top: calc(min(1vw, 2vh) * 3);
     margin-bottom: calc(min(1vw, 2vh) * 5);
     margin-right: calc(min(1vw, 2vh) * 2.5);
     font-size: calc(min(1vw, 2vh) * 4);
     line-height: calc(min(1vw, 2vh) * 5.3);
-    color: $black;
   }
 }
 
@@ -144,13 +144,13 @@ $yellow: #fccf5a;
     margin-top: 60px;
     margin-left: 0;
     img {
-      height: 80vw;
-      width: 80vw;
+      height: calc(min(2vw, 1vh) * 40);
+      width: calc(min(2vw, 1vh) * 40);
     }
     .title-text {
-      margin-top: calc(min(2vw, 1vh) * -7.5);
-      font-size: 16vw;
-      line-height: 24vw;
+      margin-top: calc(min(2vw, 1vh) * -5);
+      font-size: calc(min(2vw, 1vh) * 8);
+      line-height: calc(min(2vw, 1vh) * 8);
     }
   }
 
@@ -162,14 +162,13 @@ $yellow: #fccf5a;
       line-height: calc(min(2vw, 1vh) * 4.8);
     }
     .date {
+      width: calc(min(2vw, 1vh) * 50);
       font-size: calc(min(2vw, 1vh) * 3.2);
       line-height: calc(min(2vw, 1vh) * 4.8);
-      margin-left: -2vw;
-      .date-text {
-        margin-left: -4vw;
-      }
+      margin: auto;
     }
     .link-to-timetable {
+      margin-top: calc(min(2vw, 1vh) * 3);
       .link-to-timetable-title {
         font-size: calc(min(2vw, 1vh) * 3.2);
         line-height: calc(min(2vw, 1vh) * 4.8);
