66ee58f6163a3652ebbbd5cb8f2633b82f6eaf93
Mencotton <mencotton0410@gmail.com>
2021-01-09 07:43:49 +0900

    Add Space for Memo  on Nazotoki

--- components/header.vue
+++ components/header.vue
@@ -16,7 +16,7 @@
           </div>
           <div class="menu-item">
             <img class="menu-icon" src="@/static/icon/help-circle.svg" />
-            <a class="menu-text" href="/nazotoki/">謎解き</a>
+            <a class="menu-text" href="/nazotoki/" target="_blank">謎解き</a>
           </div>
           <div class="menu-item">
             <img class="menu-icon" src="@/static/icon/user.svg" />
