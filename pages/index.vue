df2851f976d4534ea99bc34d481591770c794a2d
Mencotton <mencotton0410@gmail.com>
2021-01-09 06:17:14 +0900

    Minor Fix

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,12 @@
       <div class="when">
         <div class="when-title">開催日時</div>
         <ul class="date">
-          <li><div class="date-text">2021.01.09(土) 09:00 ~ 17:00</div></li>
-          <li><div class="date-text">2021.01.10(日) 09:00 ~ 16:30</div></li>
+          <li>
+            <div class="date-text">2021.01.09(土) 09:00 ~ 17:00</div>
+          </li>
+          <li>
+            <div class="date-text">2021.01.10(日) 09:00 ~ 16:30</div>
+          </li>
         </ul>
         <div class="link-to-detail">
           <div class="link-to-detail-title">オンライン企画はこちら！</div>
@@ -69,10 +73,12 @@ $yellow: #fccf5a;
   width: calc(min(1vw, 2vh) * 45);
   margin-top: calc((100vh - calc(min(1vw, 2vh) * 55)) / 2);
   margin-left: 5vw;
+
   img {
     height: calc(min(1vw, 2vh) * 42.5);
     width: calc(min(1vw, 2vh) * 42.5);
   }
+
   .title-text {
     margin-top: calc(min(1vw, 2vh) * -4.5);
     padding-right: calc(min(1vw, 2vh) * 1.5);
@@ -90,39 +96,47 @@ $yellow: #fccf5a;
   margin-left: auto;
   margin-right: 5vw;
   color: $black;
+
   .when-title {
     margin-right: calc(min(1vw, 2vh) * 2.5);
     font-size: calc(min(1vw, 2vh) * 3.5);
     line-height: calc(min(1vw, 2vh) * 5);
   }
+
   .date {
     text-align: center;
     width: calc(min(1vw, 2vh) * 45);
     font-size: calc(min(1vw, 2vh) * 2.5);
     line-height: calc(min(1vw, 2vh) * 4.2);
     color: $yellow;
+
     .date-text {
       margin-left: calc(min(1vw, 2vh) * -7);
       color: $black;
     }
   }
+
   .link-to-detail {
     margin-top: calc(min(1vw, 2vh) * 3);
+
     .link-to-detail-title {
       margin: auto;
       font-size: calc(min(1vw, 2vh) * 2.5);
       line-height: calc(min(1vw, 2vh) * 4.2);
     }
+
     .link {
       margin-right: calc(min(1vw, 2vh) * 1.5);
       font-size: calc(min(1vw, 2vh) * 2.5);
       line-height: calc(min(1vw, 2vh) * 4.2);
       color: $black;
     }
+
     .link:hover {
       color: $yellow;
     }
   }
+
   .coming-soon {
     @include Didot-font();
     margin-top: calc(min(1vw, 2vh) * 3);
@@ -142,10 +156,12 @@ $yellow: #fccf5a;
     width: 100vw;
     margin-top: 60px;
     margin-left: 0;
+
     img {
       height: calc(min(2vw, 1vh) * 40);
       width: calc(min(2vw, 1vh) * 40);
     }
+
     .title-text {
       margin-top: calc(min(2vw, 1vh) * -5);
       font-size: calc(min(2vw, 1vh) * 8);
@@ -156,32 +172,39 @@ $yellow: #fccf5a;
   .when {
     width: 100vw;
     margin: calc(min(2vw, 1vh) * 3) 0 0 0;
+
     .when-title {
       font-size: calc(min(2vw, 1vh) * 3.2);
       line-height: calc(min(2vw, 1vh) * 4.8);
     }
+
     .date {
       width: calc(min(2vw, 1vh) * 50);
       font-size: calc(min(2vw, 1vh) * 3.2);
       line-height: calc(min(2vw, 1vh) * 4.8);
       margin: auto;
     }
+
     .link-to-detail {
       margin-top: calc(min(2vw, 1vh) * 3);
+
       .link-to-detail-title {
         font-size: calc(min(2vw, 1vh) * 3.2);
         line-height: calc(min(2vw, 1vh) * 4.8);
       }
+
       .link {
         margin-right: calc(min(2vw, 1vh) * 0.75);
         font-size: calc(min(2vw, 1vh) * 3.2);
         line-height: calc(min(2vw, 1vh) * 4.8);
         color: $black;
       }
+
       .link:hover {
         color: $yellow;
       }
     }
+
     .coming-soon {
       margin: calc(min(2vw, 1vh) * 2) 0 0 0;
       font-size: calc(min(2vw, 1vh) * 4.8);
