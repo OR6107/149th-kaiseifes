372ba646c7c8adcbf5c4fc79538b9cf6e9487f9c
Mencotton <mencotton0410@gmail.com>
2021-01-08 16:52:37 +0900

    Banned Download Images

--- pages/mr_ms.vue
+++ pages/mr_ms.vue
@@ -18,76 +18,176 @@
         >
         <div class="photo-list">
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_1/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_1/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_1/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_1/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_1/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_1/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_1/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_1/4.jpg"
+            />
           </div>
           <div class="photo-name">01 細川朱鈴</div>
         </div>
         <div class="photo-list">
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_2/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_2/1.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_2/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_2/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_2/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_2/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_2/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_2/4.jpg"
+            />
           </div>
           <div class="photo-name">02 周舞</div>
         </div>
         <div class="photo-list">
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_3/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_3/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_3/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_3/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_3/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_3/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_3/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_3/4.jpg"
+            />
           </div>
           <div class="photo-name">03 柏村奈央</div>
         </div>
         <div class="photo-list">
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_4/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_4/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_4/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_4/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_4/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_4/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_4/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_4/4.jpg"
+            />
           </div>
           <div class="photo-name">04 夢咲楓</div>
         </div>
         <div class="photo-list">
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_5/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_5/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_5/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_5/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/ms_5/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_5/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/ms_5/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/ms_5/4.jpg"
+            />
           </div>
           <div class="photo-name">05 山本有紗</div>
         </div>
@@ -100,91 +200,211 @@
         >
         <div class="photo-list">
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_1/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_1/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_1/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_1/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_1/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_1/3.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_1/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_1/4.jpg"
+            />
           </div>
           <div class="photo-name">01 引田尚希</div>
         </div>
         <div class="photo-list">
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_2/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_2/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_2/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_2/2.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_2/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_2/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_2/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_2/4.jpg"
+            />
           </div>
           <div class="photo-name">02 千葉恒輝</div>
         </div>
         <div class="photo-list">
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_3/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_3/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_3/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_3/2.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_3/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_3/3.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_3/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_3/4.jpg"
+            />
           </div>
           <div class="photo-name">03 後藤大輝</div>
         </div>
         <div class="photo-list">
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_4/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_4/1.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_4/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_4/2.jpg"
+            />
           </div>
           <div class="yoko photo-item">
-            <img src="@/static/mr_ms/mr_4/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_4/3.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_4/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_4/4.jpg"
+            />
           </div>
           <div class="photo-name">04 後藤雅稀</div>
         </div>
         <div class="photo-list">
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_5/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_5/1.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_5/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_5/2.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_5/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_5/3.jpg"
+            />
           </div>
           <div class="tate photo-item">
-            <img src="@/static/mr_ms/mr_5/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_5/4.jpg"
+            />
           </div>
           <div class="photo-name">05 鎌仲義紀</div>
         </div>
         <div class="photo-list">
           <div class="square photo-item">
-            <img src="@/static/mr_ms/mr_6/1.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_6/1.jpg"
+            />
           </div>
           <div class="square photo-item">
-            <img src="@/static/mr_ms/mr_6/2.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_6/2.jpg"
+            />
           </div>
           <div class="square photo-item">
-            <img src="@/static/mr_ms/mr_6/3.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_6/3.jpg"
+            />
           </div>
           <div class="square photo-item">
-            <img src="@/static/mr_ms/mr_6/4.jpg" />
+            <img
+              oncontextmenu="return false;"
+              onselectstart="return false;"
+              onmousedown="return false;"
+              src="@/static/mr_ms/mr_6/4.jpg"
+            />
           </div>
           <div class="photo-name">06 片岡孝介</div>
         </div>
