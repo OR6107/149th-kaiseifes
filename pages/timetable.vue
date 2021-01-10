5045cea16ed3a78a636efae9f9ec77e212e93857
Mencotton <mencotton0410@gmail.com>
2021-01-10 10:32:41 +0900

    Update 後夜祭

--- pages/timetable.vue
+++ pages/timetable.vue
@@ -43,8 +43,8 @@
           <div class="content-text">中後夜</div>
         </div>
         <p>
-          中夜祭: 1/9 (土) 14:55 ~ 16:25 【終了】<br />
-          後夜祭: 1/10 (日) 11:30 ~ 13:30 <br />
+          中夜祭: 1/9 (土) 14:55 ~ 【終了】<br />
+          後夜祭: 1/10 (日) 11:15 ~ <br />
           に
           <a href="https://www.youtube.com/channel/UCd4nufEmpABSr1hdLWqWj6g"
             >YouTube Live</a
